c053630c471f9a949ff8b8745aca24430d678ae8
Pellennen <pelle_9z@gotmail.com>
2020-01-13 17:16:35 +0100

    first commit

---
+++ src/__tests__/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { mount,render } from 'enzyme';
+import App from '../components/App';
+
+test('renders the app', () => {
+  render(<App />);
+});
+
+test('fetches current persona', () => {
+    const wrapper = mount(<App />)
+    expect(wrapper.state('currentPersona')).toBe('Zac')
+});
+
+test('Page should change on click and change back', () => {
+  const wrapper = mount(<App />)
+  wrapper.find('Button').simulate('click');
+  expect(wrapper.state('currentPage')).toContain('bot')
+  wrapper.find('Button').simulate('click');
+  expect(wrapper.state('currentPage')).toContain('home')
+})
